1e7270d09f7b89a6c8b6e9e9694635eb6105f79a
Usama23156 <usamaelkady25@gmail.com>
2026-01-26 06:03:23 +0200

    ok

--- app/order-confirmation/page.tsx
+++ app/order-confirmation/page.tsx
@@ -1,34 +1,24 @@
-"use client"; // مهم جداً عشان نقدر نستخدم useTranslation و useSearchParams
+"use client";
+import { useSearchParams } from "next/navigation";
 
-import { useSearchParams, useRouter } from "next/navigation";
-import useTranslation from "@/hooks/useTranslation";
 
 export default function OrderConfirmation() {
-  const { t, lang } = useTranslation();
-  const params = useSearchParams();
-  const router = useRouter();
 
-  // رقم الطلب من الـ URL
-  const orderNumber = params.get("orderNumber") || "غير محدد";
+    const params = useSearchParams();
+const order= params.get("orderNumber") || "غير محدد";
+
+
 
   return (
-    <div className="flex items-center justify-center min-h-screen px-4">
-      <div className="border border-(--bg-color) p-8 rounded-xl shadow-md text-center max-w-md w-full">
-        <h1 className="text-2xl font-bold mb-4 text-(--text-color)">
-          {t.orderDone}
+    <div className="flex items-center justify-center min-h-screen text-center">
+      <div>
+        <h1 className="text-2xl font-bold mb-4">
+          تم تأكيد الطلب ✅
         </h1>
-        <p className="text-lg mb-2 text-(--text-color)">{t.time}</p>
-        <p className="text-(--text-color)">
-          {t.orderNum}: {orderNumber}
+        <p className="text-lg">
+          سوف يتم توصيل الطلب خلال ساعة ⏱️
         </p>
-
-        {/* زر الرجوع للصفحة الرئيسية */}
-        <button
-          onClick={() => (window.location.href = "/")}
-          className="mt-6 px-6 py-2 bg-(--bg-color) text-white rounded-lg hover:bg-red-800 transition cursor-pointer"
-        >
-          {lang === "ar" ? "العودة للصفحة الرئيسية" : "Back to Home"}
-        </button>
+        <p>رقم الطلب: {order}</p>
       </div>
     </div>
   );
