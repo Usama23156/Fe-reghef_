980fec739f2eb4e0324cee0444ad8564da41980d
Usama23156 <usamaelkady25@gmail.com>
2026-01-26 05:28:35 +0200

    last

--- app/order-confirmation/page.tsx
+++ app/order-confirmation/page.tsx
@@ -1,24 +1,7 @@
-"use client"; // لازم تكون أول سطر
+"use client";
+import React from "react";
+import ClientOrderConfirmation from "@/_component/ClientOrderConfirmation/page";
 
-import { useSearchParams } from "next/navigation";
-import useTranslation from "@/hooks/useTranslation";
-
-export default function OrderConfirmation() {
-  const { t } = useTranslation();
-  const params = useSearchParams(); // client-only
-  const order = params.get("orderNumber") || "غير محدد";
-
-  return (
-    <div className="flex items-center justify-center min-h-screen text-center px-4">
-      <div>
-        <h1 className="text-2xl font-bold mb-4 text-(--text-color)">
-          {t.orderDone}
-        </h1>
-        <p className="text-lg mb-2 text-(--text-color)">{t.time}</p>
-        <p className="text-(--text-color)">
-          {t.orderNum} {order}
-        </p>
-      </div>
-    </div>
-  );
+export default function Page() {
+  return <ClientOrderConfirmation />;
 }
