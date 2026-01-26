763269051fec3fbe0643ac6a9c3baec597b942ba
Usama23156 <usamaelkady25@gmail.com>
2026-01-26 05:03:46 +0200

    last

--- app/order-confirmation/page.tsx
+++ app/order-confirmation/page.tsx
@@ -1,25 +1,23 @@
 "use client";
+
 import { useSearchParams } from "next/navigation";
 import useTranslation from "@/hooks/useTranslation";
 
-
 export default function OrderConfirmation() {
- const { t } = useTranslation();
-    const params = useSearchParams();
-const order= params.get("orderNumber") || "غير محدد";
-
-
+  const { t, lang } = useTranslation();
+  const params = useSearchParams();
+  const order = params.get("orderNumber") || "غير محدد";
 
   return (
-    <div className="flex items-center justify-center min-h-screen text-center">
+    <div className="flex items-center justify-center min-h-screen text-center px-4">
       <div>
-        <h1 className="text-2xl font-bold mb-4 text-(--text-color) ">
-         {t.orderDone}
+        <h1 className="text-2xl font-bold mb-4 text-(--text-color)">
+          {t.orderDone}
         </h1>
-        <p className="text-lg text-(--text-color)">
-         {t.time}
+       
+        <p className="text-(--text-color)">
+          {t.orderNum} {order}
         </p>
-        <p className="text-(--text-color)">{t.orderNum} {order}</p>
       </div>
     </div>
   );
