e828746d1312cd3c260858eea619cc4fcc8f1248
Usama23156 <usamaelkady25@gmail.com>
2026-01-26 05:14:57 +0200

    conferm

--- app/order-confirmation/page.tsx
+++ app/order-confirmation/page.tsx
@@ -1,12 +1,12 @@
-"use client";
+"use client"; // لازم تكون أول سطر
 
-
+import { useSearchParams } from "next/navigation";
 import useTranslation from "@/hooks/useTranslation";
 
-export default function OrderConfirmation({ searchParams }: { searchParams: { orderNumber?: string } }) {
-  const { t, lang } = useTranslation();
- 
-  const order = searchParams.orderNumber|| "غير محدد";
+export default function OrderConfirmation() {
+  const { t } = useTranslation();
+  const params = useSearchParams(); // client-only
+  const order = params.get("orderNumber") || "غير محدد";
 
   return (
     <div className="flex items-center justify-center min-h-screen text-center px-4">
@@ -14,7 +14,7 @@ export default function OrderConfirmation({ searchParams }: { searchParams: { or
         <h1 className="text-2xl font-bold mb-4 text-(--text-color)">
           {t.orderDone}
         </h1>
-       
+        <p className="text-lg mb-2 text-(--text-color)">{t.time}</p>
         <p className="text-(--text-color)">
           {t.orderNum} {order}
         </p>
