94d3c244160e23410c9611fec7c3606d4fc5d747
Usama23156 <usamaelkady25@gmail.com>
2026-01-26 06:45:19 +0200

    ok

--- app/order-confirmation/page.tsx
+++ app/order-confirmation/page.tsx
@@ -1,36 +1,10 @@
-"use client"; 
+import { Suspense } from "react";
+import OrderConfirmationClient from "./OrderConfirmationClient";
 
-import { useSearchParams, useRouter } from "next/navigation";
-import useTranslation from "@/hooks/useTranslation";
-
-export default function OrderConfirmation() {
-  const { t, lang } = useTranslation();
-  const params = useSearchParams();
-  const router = useRouter();
-
-  // رقم الطلب من الـ URL
-  const orderNumber = params.get("orderNumber") || "غير محدد";
-
+export default function Page() {
   return (
-    <div className="flex items-center justify-center min-h-screen px-4 bg-(--bg-color)">
-      <div className="bg-white p-8 rounded-xl shadow-md text-center max-w-md w-full">
-        <h1 className="text-2xl font-bold mb-4 textr(--text-color)">
-          {t.orderDone}
-        </h1>
-        <p className="text-lg mb-2 text-(--text-color)">
-          {t.time}
-        </p>
-        <p className="text-(--text-color) mb-6">
-          {t.orderNum}: {orderNumber}
-        </p>
-
-        <button
-          onClick={() => router.push("/")}
-          className="px-6 py-2 bg-(--main-color) text-white rounded-lg hover:bg-red-700 transition"
-        >
-          {lang === "ar" ? "العودة للصفحة الرئيسية" : "Back to Home"}
-        </button>
-      </div>
-    </div>
+    <Suspense fallback={<div className="min-h-screen flex items-center justify-center">Loading...</div>}>
+      <OrderConfirmationClient />
+    </Suspense>
   );
 }
